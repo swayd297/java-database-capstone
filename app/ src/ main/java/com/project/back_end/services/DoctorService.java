53b2275f0f8af0356da01401bc72c7717e2253f7
Tran Nhat Dang <trannhatdang2972005@gmail.com>
2025-10-26 19:58:48 +0700

    Implement DoctorService for managing doctors

---
+++ app/ src/ main/java/com/project/back_end/services/DoctorService.java
@@ -0,0 +1,31 @@
+package com.project.back_end.services;
+
+import com.project.back_end.models.Doctor;
+import com.project.back_end.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Optional<Doctor> getDoctorById(Long id) {
+        return doctorRepository.findById(id);
+    }
+
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
+}
